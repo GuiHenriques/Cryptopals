a18b48e6dc984b3cf90f8143f7aae60815bcf70b
GuiHenriques <gui10ghc@gmail.com>
2023-09-18 17:07:47 -0300

    fix: buffers

--- challenge_2/fixed_xor.cpp
+++ challenge_2/fixed_xor.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <string>
 #include <sstream>
 
 int main() {
-    std::string buffer1 = "1c011";
-    std::string buffer2 = "68697";
+    std::string buffer1 = "1c0111001f010100061a024b53535009181c";
+    std::string buffer2 = "686974207468652062756c6c277320657965";
     std::string result;
     
     int buffer_length = buffer1.length(); // Buffers length
