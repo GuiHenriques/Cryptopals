4836f55b88887b1276d51e53aacdf4a1aeb1c23d
Gui Henriques <gui13ghc@gmail.com>
2023-09-17 23:41:17 -0300

    add: comments

--- challenge_2/fixed_xor.cpp
+++ challenge_2/fixed_xor.cpp
@@ -3,16 +3,21 @@
 #include <sstream>
 
 int main() {
-    std::string buffer1 = "1c0111001f010100061a024b53535009181c";
-    std::string buffer2 = "686974207468652062756c6c277320657965";
+    std::string buffer1 = "1c011";
+    std::string buffer2 = "68697";
     std::string result;
+    
+    int buffer_length = buffer1.length(); // Buffers length
 
-    for (int i = 0; i < buffer1.length(); i++) {
-        int xored_bit = std::stoi(buffer1.substr(i, 1), 0, 16) ^ std::stoi(buffer2.substr(i, 1), 0, 16);
-        
+    for (int i = 0; i < buffer_length; i++) { // For each character in buffer
+        int bit1 = std::stoi(buffer1.substr(i, 1), 0, 16); // convert char to int
+        int bit2 = std::stoi(buffer2.substr(i, 1), 0, 16); //convert char to int
+
+        int xored_bit = bit1 ^ bit2; // xor operation
+
         std::stringstream ss;
-        ss << std::hex << xored_bit;
-        result += ss.str();
+        ss << std::hex << xored_bit; // convert int to hex value
+        result += ss.str(); // append hex value to result
     }
     
     std::cout << result << std::endl;
